a89f1ff2d16c1cb9689c839c54e4cbcc6318ec15
AlexB <55703624+AleSteB@users.noreply.github.com>
2023-03-29 16:25:46 +0200

    update md converter

---
+++ Ontology_Metadata_out/AFO.md
@@ -0,0 +1,65 @@
+## AFO - Allotrope Foundation Ontology
+
+
+## Ontology
+|Aspect |Description|
+    |:--|:--|
+    | Full Name | Allotrope Foundation Ontology |
+| Synonyms/Alternative Names | Allotrope Foundation Ontologies |
+| Ontology Acronym | AFO |
+| Creator(s) & Issuing Organisation | Allotrope Foundation |
+| Nature of Organisational Structure | Industrial Consortium |
+
+## References
+|Aspect |Description|
+    |:--|:--|
+    | Organisational Website | https://www.allotrope.org/ |
+| Persistent URI of Ontology File | http://purl.allotrope.org/voc/afo/merged/REC/2021/09/merged-and-inferred |
+| Link to Documentation | Missing/not found |
+| Link to Version directory | https://allotrope.jfrog.io/ui/repos/tree/General/AFO-release-public |
+| Optional links (Papers, Repos,...) | https://www.allotrope.org/resources |
+
+## Ontology Modeling And Availability
+|Aspect |Description|
+    |:--|:--|
+    | Ontology Formats Provided | ttl |
+| Degree of Inference/Composition | Inferred & non-inferred; Compacted & non-compacted |
+| License | Creative Commons Attribution 4.0 International License |
+| Validated Resoning with | Reasonable Shortest reasoning time |
+| Shortest reasoning time | Aligned with Top Level Ontology (BFO, OBO or EMMO ?) |
+| Aligned with Top Level Ontology | BFO (https://bioportal.bioontology.org/ontologies/AFO) |
+| Imports Ontology(ies) | https://bioportal.bioontology.org/ontologies/AFO/?p=mappings |
+| Prefixes used | dcterms; skos; |
+| Class annotation types | skos:PrefLabel, rdfs:Definition |
+
+## Domain of Interest Represented (contained, related: broader/narrower, missing)
+|Aspect |Description|
+    |:--|:--|
+    | Biocatalysis | missing |
+| Heterogenous catalysis | related: broader |
+| Homogenous catalysis | related: broader |
+| Chemical Substance Modeling | related: narrower |
+| Material Modeling | contained |
+| Process Modeling | related: narrower |
+| Synthesis Data | related: broader |
+| Operando Data | related: broader |
+| Performance Data | related: broader |
+| Characterisation Data | contained |
+| Heat, Transport and Kinetic Data | related: narrower |
+| Process Design, Energy and Cost Data | related: narrower |
+| Top Level Ontology | contained BFO |
+
+## Ontology Characteristics
+|Aspect |Description|
+    |:--|:--|
+    | Axioms | 24686 |
+| Logical | 5299 |
+| Declaration | 3347 |
+| Class count | 2876 |
+| Object property count | 294 |
+| Data property count | 24 |
+| Individual count | 42 |
+| Annotation Property count | 116 |
+
+## Comments
+[]
--- convert.py
+++ convert.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import json
 
-onto_list = ['AFO','BFO','EMMO']
+onto_list = ['AFO','BFO']#,'EMMO']
 for onto_name in onto_list:
     table = pd.read_excel('./Master_Table/Possible_Template_TF_OntoWorldMap_2023-03-28_10-52.xlsx',sheet_name = onto_name)
     
     with open("./GeneralStructure.json") as f: 
-        trans_dict = json.load(f)
+        ontodata_dict = json.load(f)
     
     #a = table.Ontology.dropna(how='all')
     
@@ -16,37 +16,39 @@ for onto_name in onto_list:
     data_table = ["Ontology"]
     data_table.extend(list(table.Ontology.dropna(how='all')))
     
-    for superkey in trans_dict:
+    for superkey in ontodata_dict:
         if superkey == "Comments":
             list_index = data_table.index(superkey)
-            trans_dict[superkey]= data_table[list_index+1]
+            ontodata_dict[superkey]= data_table[list_index+1:]
         else:
-            for key in trans_dict[superkey]:        
+            for key in ontodata_dict[superkey]:        
                 list_index = data_table.index(key)
-                trans_dict[superkey][key] = data_table[list_index+1]
+                ontodata_dict[superkey][key] = data_table[list_index+1]
     
     with open("./json/"+onto_name+".json", "w") as f:
-        json.dump(trans_dict,f)
+        json.dump(ontodata_dict,f)
         
     
     with open("./md-translator.json") as f:     
         translator_dict = json.load(f)
         
-    outstring = ""
+        
+    outstring = "## " + onto_name + " - " + ontodata_dict["Ontology"]["Ontology Name"] + "\n\n\n"   
+    
+    table_string = """|Aspect |Description|
+    |:--|:--|
+    """
     
     for key in translator_dict:
         if key == "Comments":
-            
+            outstring = outstring + "## Comments\n"
+            outstring = outstring + str(translator_dict[key]) + "\n"
         else:
+            outstring = outstring + "## "+ key + "\n"
+            outstring = outstring + table_string
             for dict_list in translator_dict[key]:
-                
+                outstring = outstring + "| " + str(list(dict_list.values())[0]) + " | "+ str(ontodata_dict[key][list(dict_list.keys())[0]]) + " |\n"
+            outstring = outstring + "\n"
             
-    
-    
-    outstring = """
-    |Aspect                                |Description                                                                        |
-|:--|:--|
-"""
-
-    with open('tester.md', 'w') as f:
+    with open('./Ontology_Metadata_out/'+ onto_name +'.md', 'w') as f:
         f.write(outstring)
