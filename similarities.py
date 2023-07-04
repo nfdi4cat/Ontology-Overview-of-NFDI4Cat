5b68a69a33c6ca2a8b07fbd362b61f5c7942fb84
AlexB <55703624+AleSteB@users.noreply.github.com>
2023-07-04 12:16:37 +0200

    Update voc4cat Mapping

--- similarities.py
+++ similarities.py
@@ -698,7 +698,7 @@ def Similarity_Search_from_List(input_list):
         
 ####
 def run_similarity_from_vocabulary(): 
-    test_ontology = get_ontology('./ontologies/Photocatalysis_LIKAT_template043_final.owl').load()
+    test_ontology = get_ontology('./ontologies/voc4cat.owl').load()
     ind_list = list(test_ontology.individuals())
     prefList = [str(i.prefLabel[0]) for i in ind_list]
     Similarity_Search_from_List(prefList)
