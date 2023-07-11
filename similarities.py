888ba7ac632a1020bed8d01b2f8d877dbc84ddf5
AlexB <55703624+AleSteB@users.noreply.github.com>
2023-07-11 11:27:53 +0200

    Update similarities.py

--- similarities.py
+++ similarities.py
@@ -8,7 +8,7 @@ import json
 import urllib 
 import pandas as pd
 import convert
-
+import time
 
 import sys
 sys.setrecursionlimit(100000)
@@ -61,291 +61,34 @@ def ontology_classes_loader(ontology):
 
 ####
 
-def ontology_comparison(onto1, onto2):
-    ### DEPRECATED FUNCTION!
-    
-    # Load the two ontologies
-    #onto1 = get_ontology("file://./ontology_files/" + onto_name1).load()
-    #onto2 = get_ontology("file://./ontology_files/" + onto_name2).load()
-    onto_name1 = onto1.name
-    onto_name2 = onto2.name
-    # Create sets of class labels for each ontology
-    ## using label depending on ontology!
-    
-    # label
-    try:
-        onto1_labels = set([cls.label.first() for cls in onto1.classes()])
+def class_definition_readin(ontology_class):
+    try: 
+        if ontology_class.definition:
+            definition_string = ontology_class.definition
+        else: #causing Error to break the try
+            sys.exit(1)
     except:
-        print("Class labels of ontology " + onto_name1 + " not (well) defined and could not be read!")
-        onto1_labels= set()
-    
-    # prefLabel
-    try:
-        onto1_prefLabels = set([cls.prefLabel.first() for cls in onto1.classes()])
-    except:
-        print("Class prefLabels of ontology " + onto_name1 + " not (well) defined and could not be read!")
-        onto1_prefLabels= set()
-        
-    # altLabel
-    try:
-        onto1_altLabels = set([cls.altLabel.first() for cls in onto1.classes()])
-    except:
-        print("Class altLabels of ontology " + onto_name1 + " not (well) defined and could not be read!")
-        onto1_altLabels= set()
-        
-    # name
-    try:
-        onto1_names = set([cls.name for cls in onto1.classes()])
-    except:
-        print("Class names of ontology " + onto_name1 + " not (well) defined and could not be read!")
-        onto1_names= set()
-        
-    # Concatenate prefLabels, labels and altLabels
-    onto1_combined = ((onto1_labels.union(onto1_prefLabels)).union(onto1_altLabels)).union(onto1_names)
-    
-    ## 
-    # Onto 2: 
-    # label
-    try:
-        onto2_labels = set([cls.label.first() for cls in onto2.classes()])
-    except:
-        print("Class labels of ontology " + onto_name2 + " not (well) defined and could not be read!")
-        onto2_labels= set()
-        
-    # prefLabel
-    try:
-        onto2_prefLabels = set([cls.prefLabel.first() for cls in onto2.classes()])
-    except:
-        print("Class prefLabels of ontology " + onto_name2 + " not (well) defined and could not be read!")
-        onto2_prefLabels= set()
-        
-    # altLabel
-    try:
-        onto2_altLabels = set([cls.altLabel.first() for cls in onto2.classes()])
-    except:
-        print("Class altLabels of ontology " + onto_name2 + " not (well) defined and could not be read!")
-        onto2_altLabels = set()
-        
-    # name
-    try:
-        onto2_names = set([cls.name for cls in onto2.classes()])
-    except:
-        print("Class names of ontology " + onto_name2 + " not (well) defined and could not be read!")
-        onto2_names = set()
-        
-    # Concatenate prefLabels, labels and altLabels
-    onto2_combined = ((onto2_labels.union(onto2_prefLabels)).union(onto2_altLabels)).union(onto2_names)
-    
-    ## 
-    # Find the intersection of the two sets of class labels
-    
-    try:
-        common_labels = onto1_combined.intersection(onto2_combined)
-    except:
-        print("No common labels found for ontologies " + onto_name1 + " and " + onto_name2 +".\n Might also be caused by wrong label accessing!")
-    ######
-    
-    # delete none entries
-    common_labels = [i for i in common_labels if i is not None]
-    
-    ######
-    # Find the classes in each ontology with the common labels
-    # label
-    try:
-        onto1_classes_label = [onto1.search_one(label=label) for label in common_labels if label is not None]
-    except:
-        print("No common labels found for ontologies " + onto_name1 + " and " + onto_name2 +".\n Might also be caused by wrong label accessing! len(common_labels)= " + str(len(common_labels)))
-        print(common_labels)
-    # prefLabel
-    try:
-        onto1_classes_prefLabel = [onto1.search_one(prefLabel=label) for label in common_labels if label is not None]
-    except:
-        print("No common labels found for ontologies " + onto_name1 + " and " + onto_name2 +".\n Might also be caused by wrong label accessing! len(common_labels)= " + str(len(common_labels)))
-        print(common_labels)
-    # altLabel
-    try:
-        onto1_classes_altLabel = [onto1.search_one(altLabel=label) for label in common_labels if label is not None]
-    except:
-        print("No common labels found for ontologies " + onto_name1 + " and " + onto_name2 +".\n Might also be caused by wrong label accessing! len(common_labels)= " + str(len(common_labels)))
-        print(common_labels)
-    # name
-    try:        
-        onto1_classes_name = [onto1.search_one(name=label) for label in common_labels if label is not None]
-    except:
-        print("No common labels found for ontologies " + onto_name1 + " and " + onto_name2 +".\n Might also be caused by wrong label accessing! len(common_labels)= " + str(len(common_labels)))
-        print(common_labels)        
-    ######
-    
-    concat_list = [onto1_classes_label,onto1_classes_prefLabel,onto1_classes_altLabel,onto1_classes_name]
-    
-    onto1_classes = set().union(*concat_list)
-    onto1_classes = set([i for i in onto1_classes if i is not None])
-    # Print out the matching classes
-   # for i, cls1 in enumerate(onto1_classes):
-   #     cls2 = onto2_classes[i]
-   #     print(f"{cls1.name} ({cls1.label.first()}) in Ontology 1 matches {cls2.name} ({cls2.label.first()}) in Ontology 2")
-   
-    result_dict = {}
-    for i in common_labels:
-        labeldict1 ={"label":None,
-                     "prefLabel":None,
-                     "altLabel":None,
-                     "name":None}
-        
-        labeldict2 ={"label":None,
-                     "prefLabel":None,
-                     "altLabel":None,
-                     "name":None}
-        
-        
-        # searching for each label, prefLabel, altLabel and name in the ontology.
-        # first for label, then for prefLabel, ... and listing all the other 
-        # descriptors in the dict
         try:
-            labeldict1["label"] = {"label": onto1.search_one(label = i).label.first(),
-                                   "prefLabel": onto1.search_one(label = i).prefLabel.first(),
-                                   "altLabel": onto1.search_one(label = i).altLabel.first(),
-                                   "name": onto1.search_one(label = i).name,
-                                   "iri": onto1.search_one(label = i).get_iri(onto1.search_one(label = i))
-                                   }
+            if ontology_class.hasDefinition:
+                definition_string = ontology_class.hasDefinition
+            else:
+                sys.exit(1)
         except:
-            pass
-
-        try:
-            #labeldict1["prefLabel"] = onto1.search_one(prefLabel = i)
-            labeldict1["prefLabel"] = {"label": onto1.search_one(prefLabel = i).label.first(),
-                                       "prefLabel": onto1.search_one(prefLabel = i).prefLabel.first(),
-                                       "altLabel": onto1.search_one(prefLabel = i).altLabel.first(),
-                                       "name": onto1.search_one(prefLabel = i).name,
-                                       "iri": onto1.search_one(prefLabel = i).get_iri(onto1.search_one(prefLabel = i))
-                                       }
-        except:
-            pass
-       
-        try:
-            labeldict1["altLabel"] = {"label": onto1.search_one(altLabel = i).label.first(),
-                           "prefLabel": onto1.search_one(altLabel = i).prefLabel.first(),
-                           "altLabel": onto1.search_one(altLabel = i).altLabel.first(),
-                           "name": onto1.search_one(altLabel = i).name,
-                           "iri": onto1.search_one(altLabel = i).get_iri(onto1.search_one(altLabel = i))
-                           }
-        except:
-            pass
-        
-        try:
-            labeldict1["name"] = {"label": onto1.search_one(name = i).label.first(),
-                                   "prefLabel": onto1.search_one(name = i).prefLabel.first(),
-                                   "altLabel": onto1.search_one(name = i).altLabel.first(),
-                                   "name": onto1.search_one(name = i).name,
-                                   "iri": onto1.search_one(name = i).get_iri(onto1.search_one(name = i))
-                                   }
-        except:
-            pass
-        
-        
-        ## same with onto 2
-        try:
-            labeldict2["label"] = {"label": onto2.search_one(label = i).label.first(),
-                           "prefLabel": onto2.search_one(label = i).prefLabel.first(),
-                           "altLabel": onto2.search_one(label = i).altLabel.first(),
-                           "name": onto2.search_one(label = i).name,
-                           "iri": onto2.search_one(label = i).get_iri(onto2.search_one(label = i))
-                           }
-        except:
-            pass
-        try:
-            labeldict2["prefLabel"] = {"label": onto2.search_one(prefLabel = i).label.first(),
-                           "prefLabel": onto2.search_one(prefLabel = i).prefLabel.first(),
-                           "altLabel": onto2.search_one(prefLabel = i).altLabel.first(),
-                           "name": onto2.search_one(prefLabel = i).name,
-                           "iri": onto2.search_one(prefLabel = i).get_iri(onto2.search_one(prefLabel = i))
-                           }
-        except:
-            pass
-        
-        try:
-            labeldict2["altLabel"] = {"label": onto2.search_one(altLabel = i).label.first(),
-                           "prefLabel": onto2.search_one(altLabel = i).prefLabel.first(),
-                           "altLabel": onto2.search_one(altLabel = i).altLabel.first(),
-                           "name": onto2.search_one(altLabel = i).name,
-                           "iri": onto2.search_one(altLabel = i).get_iri(onto2.search_one(altLabel = i))
-                           }
-        except:
-            pass
-        
-        try:
-            labeldict2["name"] = {"label": onto2.search_one(name = i).label.first(),
-                           "prefLabel": onto2.search_one(name = i).prefLabel.first(),
-                           "altLabel": onto2.search_one(name = i).altLabel.first(),
-                           "name": onto2.search_one(name = i).name,
-                           "iri": onto2.search_one(name = i).get_iri(onto2.search_one(name = i))
-                           }
-        except:
-            pass
-        
-        result_dict[str(i)] = {onto_name1:labeldict1,
-                          onto_name2:labeldict2}
-   
-    
-    for label in result_dict:
-        if result_dict[str(label)][onto_name1] != result_dict[str(label)][onto_name1]:
-            print("Multiple Classes detected for label '{}' in ontologies {} and {}".format(label, onto_name1, onto_name2))
+            try:
+                if getattr(b,'IAO_0000115'):
+                    definition_string = getattr(b,'IAO_0000115')
+                else:
+                    sys.exit(1)                           
+            except:
+                try:
+                    if ontology_class.comment:
+                        definition_string = ontology_class.comment
+                    else:
+                        sys.exit(1)        
+                except:
+                    definition_string = []
             
-        else:
-            # If all entries in subdict of label, onto_name1 are same, 
-            # The entries are "condensed" to remove duplicates 
-            condensed_dict_list = []
-            for key in result_dict[str(label)][onto_name1]:
-                if result_dict[str(label)][onto_name1][key] not in condensed_dict_list and result_dict[str(label)][onto_name1][key] != None:
-                    condensed_dict_list.append(result_dict[str(label)][onto_name1][key])
-        
-            result_dict[str(label)][onto_name1] = condensed_dict_list
-            
-            # same for onto_name2
-            condensed_dict_list = []
-            for key in result_dict[str(label)][onto_name2]:
-                if result_dict[str(label)][onto_name2][key] not in condensed_dict_list and result_dict[str(label)][onto_name2][key] != None:
-                    condensed_dict_list.append(result_dict[str(label)][onto_name2][key])
-            
-            result_dict[str(label)][onto_name2] = condensed_dict_list
-    
-    ##
-    # removing empty entries: 
-    temp_dict = dict(result_dict)
-    for label in result_dict:
-        if result_dict[str(label)][onto_name1] == [] and result_dict[str(label)][onto_name2] == []:
-            del temp_dict[str(label)]
-    
-    # another cleaning step for comparison of different labeling types in list:
-    key_set = set(list(temp_dict.keys()))
-    temp_dict_out = dict(temp_dict)
-    
-    for label in temp_dict:
-        if temp_dict[label][onto_name1] == temp_dict[label][onto_name2]:
-            dict_list = []
-            for entries in temp_dict[label][onto_name1]:
-                for keys in entries:
-                    if entries[keys] != None and keys != "iri":
-                        dict_list.append(entries[keys])
-            
-            throw_list = list(key_set.intersection(dict_list))
-    # ab hier:        
-            if len(throw_list) > 1:
-                for i in list(temp_dict.keys()):
-                    for dicts in temp_dict[i][onto_name1]:
-                        if dicts["label"] != None:
-                            try:
-                                throw_list.remove(dicts["label"])
-                                for i in throw_list:
-                                    del temp_dict_out[str(i)]
-                                    key_set = set(list(temp_dict_out.keys()))
-                            except:
-                                pass        
-    
-    return onto1_classes, common_labels, temp_dict_out, result_dict
-
-####
-
+    return definition_string
 
 ####
 def get_ontology_URLs():
@@ -522,11 +265,10 @@ def store_similarities(onto_combination, match_list):
     onto2_data1 = []
     onto2_data2 = []
     
-    #get ontology URLs Dictionary
-    onto_URLs = get_ontology_URLs()
+    def_list = []
     
     #load second ontology
-   # ALEX onto2 = 
+    onto2 = load_ontology_from_name(onto_combination[1])
     
     # iterate through match_list
     for entry in match_list:
@@ -549,9 +291,11 @@ def store_similarities(onto_combination, match_list):
         
         onto2_data1.append(onto2_entry1)
         onto2_data2.append(onto2_entry2)
-    
+        
+        class_def = class_definition_readin(onto2.search_one(iri = onto2_entry1))
+        def_list.append(class_def)
     # Create DataFrame from the data
-    df = pd.DataFrame({onto_combination[0]+'_IRI': onto1_data1, onto_combination[0]+'_DESC':onto1_data2, onto_combination[1]+'_IRI': onto2_data1, onto_combination[1]+'_DESC':onto2_data2})
+    df = pd.DataFrame({onto_combination[0]+'_IRI': onto1_data1, onto_combination[0]+'_DESC':onto1_data2, onto_combination[1]+'_IRI': onto2_data1, onto_combination[1]+'_DESC':onto2_data2,onto_combination[1]+'_DEF':def_list})
     
     df.to_excel("./mapping/"+onto_combination[0]+"_"+onto_combination[1]+".xlsx")
     
@@ -710,6 +454,16 @@ def run_similarity_from_vocabulary():
     prefList = [str(i.prefLabel[0]) for i in ind_list]
     Similarity_Search_from_List(prefList,"input_list")
 ####
+
+t = time.time()
+
+df = pd.read_excel("Combined Cleaned Vocabulary.xlsx", sheet_name = "Concepts", skiprows=1)
+column_data = list(df["Preferred Label*"])
+data_frame_numbers = Similarity_Search_from_List(column_data,"Combined Cleaned Vocabulary")
+
+elapsed = time.time() - t
+
+print(elapsed)
 
 
 """
